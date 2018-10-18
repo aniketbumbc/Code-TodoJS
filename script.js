ed4a873aef6f4540cbaabd061ce07668151b9ed5
aniket umbc <aniket.umbc@gmail.com>
2018-10-18 17:48:04 +0530

    Added select all n unselect in JS

--- script.js
+++ script.js
@@ -3,19 +3,23 @@ console.log("Hello test");
 var todosList = {
     todos: [],
     displayTodo: function () {
-        if (this.todos.length == 0) {
+        if (this.todos.length === 0) {
             console.log("Please Add item in List");
         } else {
             console.log('MyList :-');
             for (var i = 0; i < this.todos.length; i++) {
-                console.log(this.todos[i].todoText);
+                if (this.todos[i].compeleted === true)
+                    console.log("(x)" + this.todos[i].todoText);
+                else {
+                    console.log("( )" + this.todos[i].todoText);
+                }
             }
         }
     },
     addTodo: function (itemText) {
         this.todos.push({
             todoText: itemText,
-            compeleted: true
+            compeleted: false
         });
         this.displayTodo();
     },
@@ -30,6 +34,28 @@ var todosList = {
     toggledCompleted: function (position) {
         var todo = this.todos[position];
         todo.compeleted = !todo.compeleted;
+        this.displayTodo();
+    },
+    toggledAll: function () {
+        ////everything is true make everything true
+        
+        var totolTodos = this.todos.length;
+        var compeletedTodo = 0;
+        for (var i = 0; i < totolTodos; i++) {
+            if (this.todos[i].compeleted === true) {
+                compeletedTodo++;
+            }
+        }
+        //everything is true make everything false
+        if (compeletedTodo === totolTodos) {
+            for (var i = 0; i < totolTodos; i++) {
+                this.todos[i].compeleted = false;
+            }
+        }else{
+            for (var i = 0; i < totolTodos; i++) {
+                this.todos[i].compeleted = true;
+            }
+        }
         this.displayTodo();
     }
 };
