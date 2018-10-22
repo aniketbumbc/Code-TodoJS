91b69bbf44cae771c52c5464aa89c34799bfa63c
aniket umbc <aniket.umbc@gmail.com>
2018-10-22 23:54:43 +0530

    exit from display todo in console

--- script.js
+++ script.js
@@ -2,46 +2,25 @@ console.log("Hello test");
 
 var todosList = {
     todos: [],
-    displayTodo: function () {
-
-        if (this.todos.length === 0) {
-
-            console.log("Please Add item in List");
-        } else {
-            console.log('MyList :-');
-            for (var i = 0; i < this.todos.length; i++) {
-                if (this.todos[i].compeleted === true)
-                    console.log("(x)" + this.todos[i].todoText);
-                else {
-                    console.log("( )" + this.todos[i].todoText);
-                }
-            }
-        }
-    },
     addTodo: function (itemText) {
 
         this.todos.push({
             todoText: itemText,
             compeleted: false
         });
-        this.displayTodo();
     },
     changeTodo: function (position, itemText) {
         this.todos[position].todoText = itemText;
-        this.displayTodo();
     },
     deleteTodo: function (position) {
         this.todos.splice(position, 1);
-        this.displayTodo();
     },
     toggledCompleted: function (position) {
         var todo = this.todos[position];
         todo.compeleted = !todo.compeleted;
-        this.displayTodo();
     },
     toggledAll: function () {
         ////everything is true make everything true
-
         var totolTodos = this.todos.length;
         var compeletedTodo = 0;
         for (var i = 0; i < totolTodos; i++) {
@@ -59,18 +38,15 @@ var todosList = {
                 this.todos[i].compeleted = true;
             }
         }
-        this.displayTodo();
     }
 };
 
 var handlers = {
-    displayTodo: function () {
-        todosList.displayTodo();
-    },
     addTodo: function () {
         itemText = document.getElementById('add');
         todosList.addTodo(itemText.value);
         itemText.value = ' ';
+        view.displayTodo();
     },
     changeTodo: function () {
         position = document.getElementById('position');
@@ -78,36 +54,39 @@ var handlers = {
         todosList.changeTodo(position.value, itemText.value);
         itemText.value = ' ';
         position.value = ' ';
+        view.displayTodo();
     },
     deleteTodo: function () {
         position = document.getElementById('delete');
         todosList.deleteTodo(position.value);
         position.value = ' ';
+        view.displayTodo();
     },
     toggledCompleted: function () {
         position = document.getElementById('toggledDone');
         todosList.toggledCompleted(position.value);
         position.value = ' ';
+        view.displayTodo();
     },
     toggledAll: function () {
         todosList.toggledAll();
+        view.displayTodo();
     }
 };
 
 var view = {
-    displayTodo:function(){       
+    displayTodo: function () {
         var todoUl = document.querySelector('ul');
         todoUl.innerHTML = ' ';
-        for(var i=0; i<todosList.todos.length; i++ ){          
+        for (var i = 0; i < todosList.todos.length; i++) {
             var todoLi = document.createElement('li');
-            var todo = todosList.todos[i].compeleted;
-
+            var todo = todosList.todos[i];
             var textWithCompleted = '';
-            if(todo === true)
-            {
-                textWithCompleted = '(x)' + todo.todoText;
-            }else{
-                textWithCompleted = '( )' + todo.todoText; 
+
+            if (todo.compeleted === true) {
+                textWithCompleted = '( X )' + todo.todoText;
+            } else {
+                textWithCompleted = '( )' + todo.todoText;
             }
             todoLi.textContent = textWithCompleted;
             todoUl.appendChild(todoLi);
